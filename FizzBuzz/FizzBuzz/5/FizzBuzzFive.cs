dd371f0b5cc17d43dacdacfabe39cce8c4ed1313
RamónMN <ramon.martinez.nieto@gmail.com>
2021-06-19 17:35:49 +0200

    FizzBuzzKata iteration six

---
+++ FizzBuzz/FizzBuzz/5/FizzBuzzFive.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Return Fizz if the number is divisible by 3.
+ * Return Buzz  if the number is divisible by 5.
+ * Return FizzBuzz  if the number is divisible by 3 & 5.
+ */
+
+using System;
+
+namespace TestFizzBuzz._5
+{
+    public class FizzBuzzFive
+    {
+        public const int LENGHT_ARRAY = 100;
+
+        public string[] GetResultsInArray()
+        {
+            string[] arrayFilled = new string[LENGHT_ARRAY];
+
+            for (int i = 0; i < arrayFilled.Length; i++) 
+            {
+                if ((i + 1) % 15 == 0) arrayFilled[i] = "FizzBuzz";
+                else if ((i + 1) % 5 == 0) arrayFilled[i] = "Buzz";
+                else if ((i + 1) % 3 == 0) arrayFilled[i] = "Fizz";
+                else arrayFilled[i] = (i + 1).ToString();
+            }
+
+            return arrayFilled;
+        }
+    }
+}
---
+++ FizzBuzz/TestFizzBuzz/5/TestFizzBuzz.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * In list with 100 positions return a list wit the sequent rules: 
+ * 
+ * Return Fizz if the number is divisible by 3.
+ * Return Buzz  if the number is divisible by 5.
+ * Return FizzBuzz  if the number is divisible by 3 & 5.
+ * Others numbers should be the same number of Index 
+ */
+
+using NUnit.Framework;
+
+namespace TestFizzBuzz._5
+{
+    [TestFixture]
+    class TestFizzBuzz
+    {
+        FizzBuzzFive fizzBuzz;
+        string[] resultsArray; 
+
+        [SetUp]
+        public void SetUp() 
+        {
+            fizzBuzz = new FizzBuzzFive();
+            resultsArray = fizzBuzz.GetResultsInArray();
+        } 
+
+        [Test]
+        public void ArrayResultsShouldBeLength100()
+        {    
+            Assert.That(resultsArray.Length, Is.EqualTo(100));
+        }
+
+        [TestCase(2,ExpectedResult = "Fizz")]
+        [TestCase(5, ExpectedResult = "Fizz")]
+        [TestCase(8, ExpectedResult = "Fizz")]
+        public string ShouldBeFizzWhenIsDivisibleBy3(int input) 
+        {
+            return resultsArray[input];   
+        }
+
+        [TestCase(4, ExpectedResult = "Buzz")]
+        [TestCase(9, ExpectedResult = "Buzz")]
+        public string ShouldBeBuzzWhenIsDivisibleBy5(int input)
+        {
+            return resultsArray[input];
+        }
+
+        [TestCase(14, ExpectedResult = "FizzBuzz")]
+        [TestCase(29, ExpectedResult = "FizzBuzz")]
+        [TestCase(44, ExpectedResult = "FizzBuzz")]
+        public string ShouldBeFizzBuzzWhenIsDivisibleBy15(int input)
+        {
+            return resultsArray[input];
+        }
+        [TestCase(0, ExpectedResult = "1")]
+        [TestCase(1, ExpectedResult = "2")]
+        [TestCase(3, ExpectedResult = "4")]
+        public string ShouldBeTheSameNumberWhenIsNotDivisibleBy3or5or15(int input)
+        {
+            return resultsArray[input];
+        }
+
+    }
+}
