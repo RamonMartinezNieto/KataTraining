1503cdf90a4f7a32e0599c57a35de7c8709f22aa
RamónMN <ramon.martinez.nieto@gmail.com>
2021-06-19 17:19:53 +0200

    KataLeapYear 8 min

---
+++ LeapYear/KataLeapYear/KataLeapYear/6/LeapYearSix.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UnitTestKataLeapYear._6
+{
+    public class LeapYearSix
+    {
+        public bool IsLeap(int year)
+        {
+            if (year % 400 == 0) return true;
+            else if (year % 100 == 0) return false;
+            else if (year % 4 == 0) return true;
+            return false;
+        }
+    }
+}
---
+++ LeapYear/KataLeapYear/UnitTestKataLeapYear/6/TestKataLeapYear.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+/*
+ * - All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year),
+ * - All years divisible by 100 but not by 400 are NOT leap years (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),
+ * - All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016),
+ * - All years not divisible by 4 are NOT leap years (e.g. 2017, 2018, 2019).
+*/
+
+namespace UnitTestKataLeapYear._6
+{
+    [TestFixture]
+    class TestKataLeapYear
+    {
+        LeapYearSix leapYear; 
+
+        [SetUp]
+        public void SetUp() 
+        {
+            leapYear = new LeapYearSix();
+        }
+
+        [Test]
+        public void ShouldBeTrueYearsDivisibleBy400()
+        {
+            Assert.That(leapYear.IsLeap(2000), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void ShouldBeFalseYearsDivisibleBy100AndNotDivisibleBy100()
+        {
+            Assert.That(leapYear.IsLeap(1900), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void ShouldBeTrueYearsDivisibleBy4AndNotDivisibleBy100()
+        {
+            Assert.That(leapYear.IsLeap(2008), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void ShouldBeFalseYearsNotDivisibleBy4()
+        {
+            Assert.That(leapYear.IsLeap(2017), Is.EqualTo(false));
+        }
+    }
+}
