d3747566075a0805a3cf1aa3982e5f3b0bbd1835
RamónMN <ramon.martinez.nieto@gmail.com>
2022-07-02 01:16:40 +0200

    First test, create dniKata

---
+++ DniKata/DniKata.Test.Unit/TestDni.cs
@@ -0,0 +1,18 @@
+namespace DniKata.Test.Unit;
+
+public class TestDni
+{
+
+    [Fact]
+    public void Should_Create_DniKata()
+    {
+        var dni = new DniKata("31970165G"); //happy path
+    }
+}
+
+public class DniKata
+{
+    public DniKata(string dniValue)
+    {
+    }
+}
