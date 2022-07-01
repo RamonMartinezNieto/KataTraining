808e1de653caa0dba91864fa289f14167c26e70f
RamónMN <ramon.martinez.nieto@gmail.com>
2022-07-02 01:28:38 +0200

    throw argumentException if dniValue is empty or null

--- DniKata/DniKata.Test.Unit/TestDni.cs
+++ DniKata/DniKata.Test.Unit/TestDni.cs
@@ -2,10 +2,24 @@ namespace DniKata.Test.Unit;
 
 public class TestDni
 {
-
+    //[Fact]
+    //public void Should_Create_DniKata()
+    //{
+    //    var dni = new DniKata("31970165G"); //happy path
+    //}
+    
     [Fact]
-    public void Should_Create_DniKata()
+    public void ShouldThrowArgumentException_IfString_IsEmptyOrNull()
     {
-        var dni = new DniKata("31970165G"); //happy path
+        Action actEmpty = () => new DniKata(string.Empty);
+        Action actNull = () => new DniKata(null);
+        
+        actEmpty.Should()
+            .Throw<ArgumentException>()
+            .WithMessage("dniValue should contains value");
+
+        actNull.Should()
+            .Throw<ArgumentException>()
+            .WithMessage("dniValue should contains value");
     }
 }
--- DniKata/DniKata/DniKata.cs
+++ DniKata/DniKata/DniKata.cs
@@ -4,5 +4,7 @@ public class DniKata
 {
     public DniKata(string dniValue)
     {
+        if (string.IsNullOrEmpty(dniValue))
+            throw new ArgumentException("dniValue should contains value");
     }
 }
