40859724d4041e2233aa65582fd0f4120526be8d
RamónMN <ramon.martinez.nieto@gmail.com>
2022-07-03 11:56:49 +0200

    First test to implement Dni ValueObject

--- DniKata/DniKata.Test.Unit/TestDni.cs
+++ DniKata/DniKata.Test.Unit/TestDni.cs
@@ -1,7 +1,33 @@
 namespace DniKata.Test.Unit;
 
+// DNI "documento nacional de identidad"  'identity national document' (in spain) 
+// NIE: "numero de identidad de extranjero" 'foreign identity number' (in spain)
+//
+// Rules:
+// Length 9
+// First 8 characters are numbers 
+// Last one is a char 
+// Last one is a control character, excluded U I O Ñ
+// Exception NIE: first character is X Y Z 
+// X = 0, Y = 1, Z = 2
+
+//SadPath: 00000001K  | 00000002L
+//HappyPath: 34272318H  |  Z5090857L
+
 public class TestDni
 {
+    //First tests are to implement the clas
+    [Fact]
+    public void ShouldCreateDniValueObject() 
+    {
+        Dni dni = new("00000001K");
+    }
 
+}
 
+internal class Dni
+{
+    public Dni(string v)
+    {
+    }
 }
